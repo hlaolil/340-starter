4de927af74e983c263df4a044263cd5924192f04
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-07-20 20:33:59 +0000

    Update assignment2.sql

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,28 +1,28 @@
--- Task 1: Insert new record into account table
+-- Task 1: 
 INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
--- Task 2: Modify Tony Stark record to change account_type to Admin
+-- Task 2: 
 UPDATE account 
 SET account_type = 'Admin'
 WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
 
--- Task 3: Delete Tony Stark record
+-- Task 3: 
 DELETE FROM account 
 WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
 
--- Task 4: Modify GM Hummer description using REPLACE
+-- Task 4: 
 UPDATE inventory 
 SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 
--- Task 5: Select make, model, and classification name for Sport category
+-- Task 5: 
 SELECT inventory.inv_make, inventory.inv_model, classification.classification_name
 FROM inventory
 INNER JOIN classification ON inventory.classification_id = classification.classification_id
 WHERE classification.classification_name = 'Sport';
 
--- Task 6: Update inv_image and inv_thumbnail to add /vehicles to file path
+-- Task 6: 
 UPDATE inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
