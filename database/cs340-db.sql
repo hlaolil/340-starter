af2f940688c69b1322c51ecd016750d733e143f8
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-07-28 13:51:50 +0200

    Update inventory in cs340-db.sql

--- database/cs340-db.sql
+++ database/cs340-db.sql
@@ -241,4 +241,14 @@ VALUES   (
     5
   );
 
+-- Task 4: 
+UPDATE inventory 
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- Task 6: 
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
 
