82965640f7963022f6e471fcc6533695fc23ff3b
hlaolil <tlokza@gmail.com@>
2025-07-29 13:16:36 +0200

    add error handling

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const utilities = require('./utilities/index');
 
 /* ***********************
  * View Engine and Templates
@@ -27,10 +28,30 @@ app.get("/", function(req, res) {
   res.render("index", { title: "Home" });
 });
 
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
+
 const port = process.env.PORT
 const host = process.env.HOST
 
