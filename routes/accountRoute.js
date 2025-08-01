02745440db1e26e7b298897a3d58ac2ad171e8f3
hlaolil <hla24002@byui.edu>
2025-08-01 17:27:41 +0000

    add util.handleErrors

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,9 +2,10 @@
 const express = require("express")
 const router = new express.Router() 
 const accController = require("../controllers/accController")
-const utilities = require("../utilities/index")
+const utilities = require("../utilities/index");
+const Util = require("../utilities/index");
 
 // Route to build specific inventory item detail view
-router.get("/login", accController.buildLogin);
+router.get("/login", Util.handleErrors(accController.buildLogin));
 
 module.exports = router;
--- utilities/index.js
+++ utilities/index.js
@@ -85,4 +85,14 @@ Util.formatVehicleDetailHTML = async function (vehicle) {
   `;
 };
 
+Util.handleErrors = function (fn) {
+  return async (req, res, next) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
 module.exports = Util;
